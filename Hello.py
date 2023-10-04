ed671619962ad75a2f25c332342ad9624985a0ae
Mew0333 <146982652+Mew0333@users.noreply.github.com>
2023-10-04 10:47:23 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -22,7 +22,7 @@ if np.any(canvas_result.image_data):
     # Display prediction
     st.header('Prediction:')
     st.markdown('This number appears to be a \n # :red[' + str(prediction) + ']')
-    st.balloons()
+    st.snow()
 else:
     # Display message if canvas is empty
     st.header('Prediction:')
